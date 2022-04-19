c6b7c0f8f6a77e298736a692cb16c516a0fece76
kota <52403688+yashikota@users.noreply.github.com>
2022-04-20 08:43:46 +0900

    フォローバック機能の追加

--- bot.py
+++ bot.py
@@ -4,6 +4,30 @@ from dotenv import load_dotenv
 import os
 import datetime
 
+
+def post_tweet(date, api):
+    # 画像のパスを取得
+    img = "./img/" + date + ".png"
+
+    # 年月日のフォーマットを変更
+    date = date.replace("_0", "_")
+    date = date.split("_")
+    date = date[0] + "年" + date[1] + "月" + date[2] + "日" + date[3] + "時"
+
+    # ツイート
+    text = date + "のWord Cloudです"
+    api.update_status_with_media(text, filename=img)
+
+
+def follow_back(api):
+    # フォローしているユーザーを取得
+    follower_list = api.get_follower_ids(count=1000)
+
+    # フォローしているユーザーをフォロー
+    for i in range(len(follower_list)):
+        api.create_friendship(user_id=follower_list[i])
+
+
 def main():
     # Word Cloudを生成
     generate.main()
@@ -17,16 +41,8 @@ def main():
     DATE = datetime.datetime.now(datetime.timezone(
         datetime.timedelta(hours=+9))).strftime("%Y_%m_%d_%H")
 
-    # 画像のパスを取得
-    img = "./img/" + DATE + ".png"
-
-    # 年月日のフォーマットを変更
-    date = DATE.replace("_0", "_")
-    date = date.split("_")
-    date = date[0] + "年" + date[1] + "月" + date[2] + "日" + date[3] + "時"
-
-    text = date + "のWord Cloudです"
-    api.update_status_with_media(text, filename=img)
+    post_tweet(DATE, api)
+    follow_back(api)
 
 
 if __name__ == "__main__":
