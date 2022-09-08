c171dcc25ff32b51f5dcfd9aa30d78661d36c4bb
kota <52403688+yashikota@users.noreply.github.com>
2022-09-08 15:42:09 +0900

    fix: エラー修正

--- bot.py
+++ bot.py
@@ -10,13 +10,14 @@ def post_tweet(count, path, api):
     img = f"./img/{path}.png"
 
     # # 年月日と時刻を取得
-    # date = datetime.datetime.now(datetime.timezone(
-    #     datetime.timedelta(hours=+9))).strftime("%Y年%m月%d日%H時")
-    # # テキストを生成
-    # text = date + "のWordCloudです\n" + str(count) + "件のツイートを解析しました"
+    date = datetime.datetime.now(datetime.timezone(
+        datetime.timedelta(hours=+9))).strftime("%Y年%m月%d日")
+
+    # テキストを生成
+    text = date + "のWordCloudです\n" + str(count) + "件のツイートを解析しました"
 
     # ツイート
-    api.update_status_with_media(filename=img)
+    api.update_status_with_media(text, filename=img)
 
 
 def follow_back(api):
